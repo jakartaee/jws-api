351d8d65798996ca45349e6f2b183e0a7168b617
Tomas Kraus <Tomas.Kraus@oracle.com>
2019-07-22 11:29:59 +0200

    Modified spec build arguments.
    Signed-off-by: Tomas Kraus <Tomas.Kraus@oracle.com>

--- etc/jenkins/continuous.sh
+++ etc/jenkins/continuous.sh
@@ -13,6 +13,6 @@
 #  N/A
 
 echo '-[ Jakarta Web Services Metadata Specification Build ]--------------------------'
-(cd spec && mvn -U -C -Psnapshots,oss-release clean install deploy)
+(cd spec && mvn -U -C -Psnapshots,oss-release -Dstatus='DRAFT' clean install deploy)
 echo '-[ Jakarta Web Services Metadata API Build ]------------------------------------'
 (cd api && mvn -U -C -Psnapshots,oss-release clean install spotbugs:spotbugs deploy)
